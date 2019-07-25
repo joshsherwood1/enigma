cb4efca950cda0ff78954a5127f02cbdb44015bd
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-25 15:35:17 -0600

    Add code to encrypt runner file to upcase message.txt for testing purposes

--- encrypt.rb
+++ encrypt.rb
@@ -4,18 +4,29 @@ require './lib/classes/shift'
 require './lib/classes/enigma'
 
 puts "You are creating a cipher using the enigma machine. Please enter something to cipher:"
-user_message = gets.chomp.downcase
-message1 = File.open(ARGV[0], "w")
-writer.write(user_message)
-writer.close
-
-# File.write('message.txt', user_message)
-
 message = File.open(ARGV[0], "r")
 incoming_message = message.read
 message.close
-capitalized_message = incoming_message.downcase
+
+capitalized_message = incoming_message.upcase
 writer = File.open(ARGV[1], "w")
 writer.write(capitalized_message)
 writer.close
+puts "Created '#{ARGV[1]}' with key .... and date ..."
+time1 = Time.new
+puts "Current Time : " + time1.inspect
+
+# message1 = File.open(ARGV[0], "w")
+# writer.write(message1)
+# writer.close
+
+# File.write('message.txt', user_message)
+
+# message = File.open(ARGV[0], "r")
+# incoming_message = message.read
+# message.close
+# capitalized_message = incoming_message.downcase
+# writer = File.open(ARGV[1], "w")
+# writer.write(capitalized_message)
+# writer.close
 puts "Created '#{ARGV[1]}' with the key .... and date ..."
