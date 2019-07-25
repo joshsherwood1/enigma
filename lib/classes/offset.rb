c6c8d3069da3de112bd3e921c984af215ab22f5f
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-25 15:35:17 -0600

    Complete method that assigns letters to digits

--- lib/classes/offset.rb
+++ lib/classes/offset.rb
@@ -30,10 +30,10 @@ class Offset
 
   def assign_letters_to_offset_digits
     key_letter_hash = Hash.new(0)
-    key_letter_hash[:A] = determine_the_offset_to_use[0..1]
-    key_letter_hash[:B] = determine_the_offset_to_use[1..2]
-    key_letter_hash[:C] = determine_the_offset_to_use[2..3]
-    key_letter_hash[:D] = determine_the_offset_to_use[3..4]
+    key_letter_hash[:A] = determine_the_offset_to_use[0]
+    key_letter_hash[:B] = determine_the_offset_to_use[1]
+    key_letter_hash[:C] = determine_the_offset_to_use[2]
+    key_letter_hash[:D] = determine_the_offset_to_use[3]
     key_letter_hash
   end
 end
