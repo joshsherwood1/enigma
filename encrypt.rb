4da02b27869fa68f44f216caf0426cef206dc4f4
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-25 15:35:17 -0600

    Add code to encrypt file so that it can read and change a text file

--- encrypt.rb
+++ encrypt.rb
@@ -1,3 +1,8 @@
-ARGV == ["message.txt", "encrypted.txt"]
-ARGV[0] == "message.txt"
-ARGV[1] == "encrypted.txt"
+message = File.open(ARGV[0], "r")
+incoming_message = message.read
+message.close
+capitalized_message = incoming_message.downcase
+writer = File.open(ARGV[1], "w")
+writer.write(capitalized_message)
+writer.close
+puts "Created '#{ARGV[1]}' with the key .... and date ..."
