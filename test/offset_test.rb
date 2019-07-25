185fdb8b3852887f20160d6d6b86c0549cc2090d
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-25 15:35:17 -0600

    Add offset test file

---
+++ test/offset_test.rb
@@ -0,0 +1,12 @@
+require './test/test_helper'
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/classes/enigma'
+require './lib/classes/key'
+require 'csv'
+require 'pry'
+require 'mocha/minitest'
+
+class OffsetTest < Minitest::Test
+end
