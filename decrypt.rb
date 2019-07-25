5275c7b3d9e6514be5dec0109e57616e91b8934f
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-25 15:35:17 -0600

    Change variable names in decrypt runner file so that they match purpose

--- decrypt.rb
+++ decrypt.rb
@@ -9,11 +9,11 @@ incoming_message = message.read.chomp.downcase
 message.close
 
 enigma = Enigma.new
-enigma.decrypt(incoming_message)
-enigma.encrypted_text
+enigma.decrypt(incoming_message, "03262", "290719")
+enigma.decrypted_text
 
-capitalized_message = enigma.encrypted_text
+decrypted_message = enigma.decrypted_text
 writer = File.open(ARGV[1], "w")
-writer.write(capitalized_message)
+writer.write(decrypted_message)
 writer.close
 puts "Created '#{ARGV[1]}' with the key #{enigma.key_object.five_digit_key} and date #{enigma.offset_object.chosen_date}"
