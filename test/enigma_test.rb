a1b817230f294703996ff719244c78e62da2a115
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-25 15:35:17 -0600

    Create shift class and test file and relocate gem lines

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,4 @@
 require './test/test_helper'
-gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/classes/enigma'
---
+++ test/shift_test.rb
@@ -0,0 +1,12 @@
+require './test/test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/classes/enigma'
+require './lib/classes/key'
+require './lib/classes/offset'
+require 'csv'
+require 'pry'
+require 'mocha/minitest'
+
+class OffsetTest < Minitest::Test
+end
