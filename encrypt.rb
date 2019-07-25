7439334d67d072eab2086f593d1e68532b303e7d
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-25 15:35:17 -0600

    Add functionality to encrypt runner file to read through class methods and change message

--- encrypt.rb
+++ encrypt.rb
@@ -2,19 +2,44 @@ require './lib/classes/key'
 require './lib/classes/offset'
 require './lib/classes/shift'
 require './lib/classes/enigma'
+require 'pry'
 
-puts "You are creating a cipher using the enigma machine. Please enter something to cipher:"
+todays_date = DateTime.now
+current_date_string = todays_date.strftime("%d%m%y").to_s
+
 message = File.open(ARGV[0], "r")
-incoming_message = message.read
+incoming_message = message.read.chomp.downcase
 message.close
+
+@key = Key.new("")
+@key.determine_the_key_to_use
+@key.five_digit_key
+
+@offset = Offset.new("")
+@offset.determine_the_offset_to_use
+@offset.four_digit_offset
+
+@shift_1 = Shift.new(@key, @offset)
+@shift_1.assign_letters_to_key_digits
+@shift_1.assign_letters_to_offset_digits
+@shift_1.make_shift_from_key_and_offset
+
+@enigma = Enigma.new(@shift_1.make_shift_from_key_and_offset)
+@enigma.encrypt(incoming_message, @key.five_digit_key, current_date_string)
+# @enigma.create_rotated_character_set_a
+# binding.pry
+
+#
+# puts "You are creating a cipher using the enigma machine. Please enter a key for the cipher:"
+# user_key = gets.chomp
 
 capitalized_message = incoming_message.upcase
 writer = File.open(ARGV[1], "w")
 writer.write(capitalized_message)
 writer.close
-puts "Created '#{ARGV[1]}' with key .... and date ..."
-time1 = Time.new
-puts "Current Time : " + time1.inspect
+puts "Created '#{ARGV[1]}' with key #{@key.five_digit_key} and date #{current_date_string}"
+# time1 = Time.new
+# puts "Current Time : " + time1.inspect
 
 # message1 = File.open(ARGV[0], "w")
 # writer.write(message1)
@@ -29,4 +54,4 @@ puts "Current Time : " + time1.inspect
 # writer = File.open(ARGV[1], "w")
 # writer.write(capitalized_message)
 # writer.close
-puts "Created '#{ARGV[1]}' with the key .... and date ..."
+# puts "Created '#{ARGV[1]}' with the key .... and date ..."
--- lib/classes/enigma.rb
+++ lib/classes/enigma.rb
@@ -37,17 +37,24 @@ class Enigma
 
   def encrypt(message, key, date)
     @message = message
-    current_date = DateTime.now
-    current_date_string = current_date.strftime("%d%m%y").to_s
-    if date.nil? == true || date == ""
-      chosen_date = current_date_string
-    else date.nil? == false
-      chosen_date = date
-    end
+    create_rotated_character_set_a
+    create_rotated_character_set_b
+    create_rotated_character_set_c
+    create_rotated_character_set_d
+    create_array_from_message
+    create_index_hash_for_a_characters
+    change_a_characters_in_message
+    create_index_hash_for_b_characters
+    change_b_characters_in_message
+    create_index_hash_for_c_characters
+    change_c_characters_in_message
+    create_index_hash_for_d_characters
+    change_d_characters_in_message
+    convert_encrypted_array_to_string
     hash = {
     encryption: convert_encrypted_array_to_string,
     key: key,
-    date: chosen_date
+    date: date
   }
   end
 
