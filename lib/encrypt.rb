864425ee675dc944db4d216025ab9595171d5788
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-25 15:35:17 -0600

    Change location of encrypt and decrypt runner files

---
+++ lib/encrypt.rb
@@ -0,0 +1,19 @@
+require './lib/classes/key'
+require './lib/classes/offset'
+require './lib/classes/shift'
+require './lib/classes/enigma'
+require 'pry'
+
+message = File.open(ARGV[0], "r")
+incoming_message = message.read.chomp.downcase
+message.close
+
+enigma = Enigma.new
+enigma.encrypt(incoming_message, "67890")
+enigma.encrypted_text
+
+capitalized_message = enigma.encrypted_text
+writer = File.open(ARGV[1], "w")
+writer.write(capitalized_message)
+writer.close
+puts "Created '#{ARGV[1]}' with the key #{enigma.key_object.five_digit_key} and date #{enigma.offset_object.chosen_date}"
